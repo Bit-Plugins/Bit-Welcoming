c82059740a771bf32de03934d0e90ff73718d076
Robin Painter <lochlanpainter@gmail.com>
2024-10-07 14:17:47 +1100

    Fixed issues with welcome function, updated the plugin.json file, removed unused files

--- welcome-newbies/events/guildMemberAdd.js
+++ welcome-newbies/events/guildMemberAdd.js
@@ -4,14 +4,13 @@ const config = require('../config.json');
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-        if(Array.isArray(config.welcome)) {
-            if(member.guild.id instanceof config.welcome) {
-                const welcome = config.welcome[member.guild.id]
-
-                if(welcome.channel) {
-                    guild.channels.cache.get(welcome.channel).send({ message: welcome.message.replace('{guildName}', member.guild.name).replace('{userMention}', '<@'+member.user.id+'>')})
-                }
-            }
-        }
+		if(config.welcome[member.guild.id]) {
+			const welcome = config.welcome[member.guild.id]
+			if(welcome.channel) {
+				member.guild.channels.cache.get(welcome.channel).send({ content: welcome.message.replace('{guildName}', member.guild.name).replace('{userMention}', '<@'+member.user.id+'>')})
+			} else {
+				console.log("Guild "+member.guild.id+" does not have a channel set")
+			}
+		}
     }
 }
