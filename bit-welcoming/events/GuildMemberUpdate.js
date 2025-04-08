e348dfa550debb4d84f013bb0812494e3491f5ce
Robin Painter <lochlanpainter@gmail.com>
2025-04-08 12:50:25 +1000

    Fixed bug with member join role

--- bit-welcoming/events/GuildMemberUpdate.js
+++ bit-welcoming/events/GuildMemberUpdate.js
@@ -9,9 +9,9 @@ module.exports = {
         const guild = oldMember.guild
 
         if(config.channels.welcome[oldMember.guild.id]) {
-            if(guild.roles.cache.get(config.channels.welcome[guild.id].role)) {
+            if(guild.roles.cache.get(config.channels.welcome[guild.id].memberRole)) {
                 if(oldMember.pending === true && newMember.pending === false) {
-                    oldMember.roles.add(config.channels.welcome[guild.id].role, 'User Joined (Discord Verification Respected)')
+                    oldMember.roles.add(config.channels.welcome[guild.id].memberRole, 'User Joined (Discord Verification Respected)')
                 }
             }
         }
