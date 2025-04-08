b7ff4339b02786db27d9dda06e1c71bf4ff1355a
Robin Painter <lochlanpainter@gmail.com>
2025-04-08 16:04:15 +1000

    Added start_function

--- bit-welcoming/index.js
+++ bit-welcoming/index.js
@@ -2,6 +2,10 @@ const core = require("bit/core");
 const { GatewayIntentBits } = require('discord.js');
 
 module.exports = {
+    start_function: function start_function() {
+        
+    },
+    
     define_intents: function define_intents() {
         core.add_intent(GatewayIntentBits.MessageContent)
         core.add_intent(GatewayIntentBits.GuildMembers)
